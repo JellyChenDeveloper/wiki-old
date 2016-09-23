2ba88f332c6bc0af23e6b9be34b4006e7ea6ab7b
JellyChen_Office <451453325@qq.com>
2016-09-23 16:59:56 +0800

    move from amazon to aliyun

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$vg6oUXOJ$B0NtConZaNmE1/zD4VIey/:JellyChen:451453325@qq.com:admin,user
+cgd451453325:$1$Xm5Q12Wp$lnhvOfGIz6ELAAD99zB/d0:Jelly:451453325@qq.com:user
